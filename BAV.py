78924855ac90aae2f0c9bd9054c41be7fcff77ca
adriaanlopz <adrianlopezrio2006@gmail.com>
2025-07-07 23:56:10 +0200

    BAV.py

--- BAV.py
+++ BAV.py
@@ -1,5 +1,8 @@
+# main.py
 import discord
 from discord.ext import commands
+import os
+import asyncio
 
 intents = discord.Intents.default()
 intents.voice_states = True
@@ -18,6 +21,12 @@ async def on_voice_state_update(member, before, after):
         canal = discord.utils.get(member.guild.text_channels, name="❰‼️❱▸alertas")
         if canal:
             await canal.send(f"🔊  **{member.display_name}** se ha conectado al canal de voz  **{after.channel.name}**.")
-            
-import os
-bot.run(os.getenv("DISCORD_BOT_TOKEN"))
+
+async def main():
+    try:
+        await bot.start(os.getenv("DISCORD_BOT_TOKEN"))
+    except Exception as e:
+        print(f"❌ Error al iniciar el bot: {e}")
+
+if __name__ == "__main__":
+    asyncio.run(main())
