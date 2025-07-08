575c0b233f1760301433b72ff20af9f2eba86e3b
Adrián López <adrianlopezrio2006@gmail.com>
2025-07-08 21:23:17 +0200

    Versión recuperada del bot BAV

---
+++ main.py
@@ -0,0 +1,23 @@
+# main.py
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Bot conectado como {bot.user}")
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if before.channel is None and after.channel is not None:
+        canal = discord.utils.get(member.guild.text_channels, name="❰🌐❱▸general")
+        if canal:
+            await canal.send(f"🔊 {member.display_name} se ha unido a {after.channel.name}")
+
+bot.run("MTM5MTcyNTcwODQ4NDU0NjU3Mg.G31NBB.JfftGcHDO5KE69lR2lJhA0drj1ctA4lGH0lF6A")
